7d92bf89ebef459f503d978bed1070e9f999a14d
Ethan <zhuyisong1994@gmail.com>
2020-09-14 16:27:15 -0700

    pte printout

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,25 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+void rec_vmprint(pagetable_t pagetable, int level) {
+	for (int i = 0; i < 512; i++) {
+		pte_t pte = pagetable[i];
+		if (pte & PTE_V) {
+			uint64 child = PTE2PA(pte);
+			for (int j = 0; j < level; j++) {
+				printf(" ..");
+			}
+			printf("%d: pte %p pa %p\n", i, pte, child);
+			if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+				rec_vmprint((pagetable_t)child, level+1);
+			} 
+		}
+	}
+}
+
+void vmprint(pagetable_t pagetable) {
+	printf("page table %p\n", pagetable);
+	rec_vmprint(pagetable, 1);
+}
